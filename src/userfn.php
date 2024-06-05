5c4e5bf0756a757d120a439043cf49f9d64f6522
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2024-06-05 12:45:14 +0700

    Update userfn.php

--- src/userfn.php
+++ src/userfn.php
@@ -32,7 +32,6 @@ function GetStartsWithAFilter($FldExpression, $dbid = "")
 }
 
 // Global user functions
-
 // Database Connecting event
 function Database_Connecting(&$info)
 {
@@ -43,6 +42,12 @@ function Database_Connecting(&$info)
     //    $info["user"] = "root";
     //    $info["password"] = "";
     //}
+    if (!IsLocal()) { // Testing on local PC
+    //    $info["host"] = "locahost";
+       $info["user"] = "u736643050_kangbani";
+       $info["dbname"] = "u736643050_kangbani";
+       $info["password"] = "PresarioCQ43@";
+    }
 }
 
 // Database Connected event
